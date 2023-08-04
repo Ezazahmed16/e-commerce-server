4f81cca7bd54e11a1804f35ce978fd384f9f6929
Ezaz Ahmed <ezazrahul794@gmail.com>
2023-08-04 21:51:17 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const morgan = require('morgan')
+const createError = require('http-errors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+
+const app = express()
+
+const rateLimiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 5, // Limit each IP to 100 requests per `window` (here, per 2 minutes)
+    message: 'Too many request from this IP. Please try again later',
+})
+
+//Middleware
+app.use(rateLimiter)
+app.use(morgan("dev"))
+app.use(xss())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        message: "Welcome to the E-commerce Server.",
+    })
+})
+
+
+// Client Error Handling: 
+app.use((req, res, next) => {
+    next(createError(404, 'router not found'))
+})
+// Server Error Handling: 
+app.use((err, req, res, next) => {
+    return res.status(err.status || 500).json({
+        success: false,
+        message: err.message,
+    })
+})
+
+module.exports = app;
